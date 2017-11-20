1a4eb54509f19ed28dc8f1d1af3a4eb9d4b89eed
Michael Vial <michaelvial@gmail.com>
2017-11-20 07:37:41 +0100

    Add access control headers

--- main.go
+++ main.go
@@ -41,6 +41,9 @@ func Index(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
+
 	json.NewEncoder(w).Encode(res)
 }
